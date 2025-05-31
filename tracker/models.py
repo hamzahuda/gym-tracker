93f8a4670f970a2906a0896f0f6361b454a4af62
hamzahuda <->
2025-05-31 19:53:04 +0100

    Create initial models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Workout(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Exercise(models.Model):
+    name = models.TextField()
+    rest_time = models.FloatField()
+    min_reps = models.IntegerField()
+    max_reps = models.IntegerField()
+
+class Set(models.Model):
+    set_number = models.IntegerField()
+    reps = models.FloatField()
+    weight = models.FloatField()
